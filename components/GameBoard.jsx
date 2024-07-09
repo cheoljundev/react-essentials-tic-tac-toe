7e1053fae2aecbcff6eed2af69659bde393b01ac
cheoljundev <cheoljundev@gmail.com>
2024-07-09 15:28:52 +0900

    조건적 버튼 비활성화

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -32,7 +32,9 @@ export default function GameBoard({onSelectSquare, turns}) {
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={()=>onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button onClick={()=>onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                            {playerSymbol}
+                        </button>
                     </li>)}
                 </ol>
             </li>
