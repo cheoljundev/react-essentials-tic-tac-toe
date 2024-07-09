34ae874d747c244038b029bd3ca951d3bb3fa346
cheoljundev <cheoljundev@gmail.com>
2024-07-09 15:46:44 +0900

    계산된 값 끌어올리기

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,42 @@ import {useState} from "react";
 import Log from "../components/Log.jsx";
 import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = "X";
-
     if (gameTurns.length > 0 && gameTurns[0].player === "X") {
         currentPlayer = "O";
-    }
 
+    }
     return currentPlayer;
 }
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
+    // const [hasWinner, setHasWinner] = useState(false);
     // const [activePlayer, setActivePlayer] = useState("X");
 
     const activePlayer = deriveActivePlayer(gameTurns);
 
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    for (const combination of WINNING_COMBINATIONS) {
+        // const firstSquareSymbol;
+        // const secondSquareSymbol;
+        // const thirdSquareSymbol;
+    }
 
     function handleSelectSquare(rowIndex, colIndex) {
         // setActivePlayer(curActivePlayer=> curActivePlayer === "X" ? "O" : "X");
@@ -42,7 +63,7 @@ function App() {
             </ol>
             <GameBoard
                 onSelectSquare={handleSelectSquare}
-                turns={gameTurns}
+                board={gameBoard}
             />
           </div>
         <Log turns={gameTurns} />
